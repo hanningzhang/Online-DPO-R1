31aa0c9eccfd8521b25887c08aa4af6f158020e1
Hanning Zhang <Hanning.zhang@outlook.com>
2025-02-11 13:31:53 +0800

    Update run_dpo.py

--- dpo_iteration/run_dpo.py
+++ dpo_iteration/run_dpo.py
@@ -136,7 +136,8 @@ def prepare_data(
 
     margin = []
     for sample in ds:
-        P = tokenizer.apply_chat_template(sample["prompt"], tokenize = False, add_generation_prompt= True)
+        #P = tokenizer.apply_chat_template(sample["prompt"], tokenize = False, add_generation_prompt= True)
+        P = sample["prompt"]
         if choose_type == "random":
             idx0 = 0
             idx1 = 1
